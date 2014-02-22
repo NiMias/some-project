90437654ba8da31effebddc4d37de4a4e99410c8
Nikolay Miasnikow <nikmyas@gmail.com>
2014-02-22 22:45:16 +0200

    added tinymce

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Nimias\Bundle\UdndBundle\UdndBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
